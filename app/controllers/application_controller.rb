2ad75a27af7e4f77c85b6772ff99bb0e83c85de5
Rachele Suy <rachelesuy@gmail.com>
2019-08-23 13:49:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < Sinatra::Base
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to '/login'
+      redirect to '/account'
     else
       redirect to '/failure'
     end
